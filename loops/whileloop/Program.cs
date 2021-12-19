77121a1c5755795b7ae03e91640183d180273017
krishpranav <krisna.pranav@gmail.com>
2021-12-19 14:55:18 +0530

    loops: while looop

--- loops/whileloop/Program.cs
+++ loops/whileloop/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WhileLoop {
+    class Loop {
+        static void Main(string[] args) {
+            int a = 10;
+
+            while (a < 20) {
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+            }
+        }
+    }
+}
