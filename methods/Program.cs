3a3cb99953d648c7714941e2714a0e9089a39b06
krishpranav <krisna.pranav@gmail.com>
2021-12-19 14:58:45 +0530

    methods

---
+++ methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator {
+    class NumberCal {
+        public int FindMax(int num1, int num2) {
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }
+
+        static void Main(string[] args) {
+            int a = 100;
+            int b = 200;
+            int ret;
+            NumberCal n = new NumberCal();
+
+            ret = n.FindMax(a, b);
+            Console.WriteLine("Max value is: {0}", ret);
+        }
+    }
+}
