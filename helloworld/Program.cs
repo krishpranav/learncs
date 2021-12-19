2f22bd5a96705959a8ade66ed55f70fd16a861db
krishpranav <krisna.pranav@gmail.com>
2021-12-19 14:16:49 +0530

    helloworld application

---
+++ helloworld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloWorldApp {
+    class HelloWorld {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
