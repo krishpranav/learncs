fdcee8bdd35f3cf8eaac31047f62edbd62f7a314
krishpranav <krisna.pranav@gmail.com>
2021-12-19 14:23:41 +0530

    datatypes

---
+++ datatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataTypes {
+    class Int {
+        static void Main(string[] args) {
+            int a = 10;
+            float b = 53.005f;
+            double c = 2345.7652;
+            bool d = true;
+
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(d.ToString());
+        }
+    }
+}
