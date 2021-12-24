d5f8e5aa4d4427574a96c11114e16b398a7d2eac
krishpranav <krisna.pranav@gmail.com>
2021-12-24 18:37:13 +0530

    enums: group of constants [enums]

--- enums/Program.cs
+++ enums/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EnumsTutorial {
+	class Program {
+		enum ProgrammingLanguage {
+			Python,
+			Rust,
+			CSharp
+		}
+
+		static void Main(string[] args)
+		{
+			ProgrammingLanguage myLanguage = ProgrammingLanguage.CSharp;
+			Console.WriteLine("Langauge used to build is: " + myLanguage);
+		}
+	}
+}
