9677c940f2af2cf88f1e516e3de20832e721422d
krishpranav <krisna.pranav@gmail.com>
2021-12-24 18:27:56 +0530

    comments

---
+++ comments/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+/**
+ * this is commented
+ */
+
+namespace Hello {
+	class HelloWorld {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World"); // this prints out line
+		}
+	}
+}
