7b5637ff47252fa05334353a6281a96d46b1eafd
krishpranav <krisna.pranav@gmail.com>
2021-12-25 11:07:37 +0530

    namespaces

--- namespaces/Program.cs
+++ namespaces/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace first_space {
+    class namespace_cl {
+        public void func() {
+            Console.WriteLine("First Namespace");
+        }
+    }
+}
+
+namespace second_space {
+    class namespace_cl {
+        public void func() {
+            Console.WriteLine("Second Namespace");
+        }
+    }
+}
+
+class TestClass {
+    static void Main(string[] args) {
+        first_space.namespace_cl fc = new first_space.namespace_cl();
+        second_space.namespace_cl sc = new second_space.namespace_cl();
+
+        fc.func();
+        sc.func();
+    }
+}
